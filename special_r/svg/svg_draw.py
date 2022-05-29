0fff1f9b79b518e9aac4a2be28b93a635d8b22c6
Grzegorz Rucki <ruckigr@gmail.com>
2022-05-29 22:30:19 +0200

    svg update

--- special_r/svg/svg_draw.py
+++ special_r/svg/svg_draw.py
@@ -1,67 +1,34 @@
 import drawSvg as draw
 
-d = draw.Drawing(200, 100, origin='center', transform='scale(4)',  displayInline=False)
 
-# Draw an irregular polygon
-lines = draw.Lines(-80, -45,
-                    70, -49,
-                    95, 49,
-                    -90, 40,
-                    close=False,
-            fill='#eeee00',
-            stroke='black'
-                    )
-#d.append(lines)
+def add_roz(surface, x, y, s):
+    rot_x, rot_y = x+(s/2), -y -(s/2)
 
-# Draw a rectangle
-for i in range(1,10):
-    r = draw.Rectangle(0,0,40,40, fill='#12{}ff'.format(str(i*10)),
-                       transform='rotate({} 20 -20) '.format(str(i*10)),
-                       )
+    for i in range(0, 10):
+        r = draw.Rectangle(x, y, s, s, fill='#12{}ff'.format(str(i * 10)),
+                           transform='rotate({} {} {}) '.format(str(i * 10), str(rot_x), str(rot_y)),
+                           )
 
-    d.append(r)
+        surface.append(r)
 
-"""
-# Draw a circle
-d.append(draw.Circle(-40, -10, 30,
-            fill='red', stroke_width=2, stroke='black'))
+def draw_roz(scale, name):
+    d = draw.Drawing(900, 900, origin='center', transform='scale({}) translate(-50 50)'.format(str(scale)),  displayInline=False)
+    d.append( draw.Rectangle(-900, -900, 1800, 1800, fill='#222222'))
+    add_roz(d, 0,10,40)
+    add_roz(d, 100, 100, 80)
+    add_roz(d, 50, 50, 20)
+    add_roz(d, -50, -50, 20)
+    d.setRenderSize(1800,1800)  # Alternative to setPixelScale
+    #d.saveSvg('{}.svg'.format(name))
+    d.savePng('out/{}.png'.format(name))
 
-# Draw an arbitrary path (a triangle in this case)
-p = draw.Path(stroke_width=2, stroke='lime',
-              fill='black', fill_opacity=0.2)
-p.M(-10, 20)  # Start path at point (-10, 20)
-p.C(30, -10, 30, 50, 70, 20)  # Draw a curve to (70, 20)
-d.append(p)
 
-# Draw text
-d.append(draw.Text('Basic text', 8, -10, 35, fill='blue'))  # Text with font size 8
-d.append(draw.Text('Path text', 8, path=p, text_anchor='start', valign='middle'))
-d.append(draw.Text(['Multi-line', 'text'], 8, path=p, text_anchor='end'))
 
-# Draw multiple circular arcs
-d.append(draw.ArcLine(60,-20,20,60,270,
-            stroke='red', stroke_width=5, fill='red', fill_opacity=0.2))
-d.append(draw.Arc(60,-20,20,60,270,cw=False,
-            stroke='green', stroke_width=3, fill='none'))
-d.append(draw.Arc(60,-20,20,270,60,cw=True,
-            stroke='blue', stroke_width=1, fill='black', fill_opacity=0.3))
+if __name__ == '__main__':
+    steps = list(range(1000)) + list(reversed(range(1000)))
 
-# Draw arrows
-arrow = draw.Marker(-0.1, -0.5, 0.9, 0.5, scale=4, orient='auto')
-arrow.append(draw.Lines(-0.1, -0.5, -0.1, 0.5, 0.9, 0, fill='red', close=True))
-p = draw.Path(stroke='red', stroke_width=2, fill='none',
-              marker_end=arrow)  # Add an arrow to the end of a path
-p.M(20, -40).L(20, -27).L(0, -20)  # Chain multiple path operations
-d.append(p)
-d.append(draw.Line(30, -20, 0, -10,
-            stroke='red', stroke_width=2, fill='none',
-            marker_end=arrow))  # Add an arrow to the end of a line
-"""
-d.setPixelScale(2)  # Set number of pixels per geometry unit
-#d.setRenderSize(400,200)  # Alternative to setPixelScale
-d.saveSvg('example.svg')
-d.savePng('example2.png')
+    for i, v in enumerate(steps):
+        print(i)
 
-# Display in Jupyter notebook
-d.rasterize()  # Display as PNG
-d  # Display as SVG
+        draw_roz(1 + 0.015*v, 'example_{}'.format(str(i).zfill(4)))
+
